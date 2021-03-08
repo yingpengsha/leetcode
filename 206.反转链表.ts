9fb51832776a9391af7046688ecf41d360b7eba4
Pengsha Ying <810998652@qq.com>
2021-03-08 11:03:54 +0800

    206.反转链表

---
+++ 206.反转链表.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head
+  const newHead = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return newHead
+};
+// @lc code=end
+

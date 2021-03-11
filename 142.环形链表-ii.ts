13ef9106c79c6e9b53a3f9dc4ffeb40f7a17c4d3
Pengsha Ying <810998652@qq.com>
2021-03-11 14:51:21 +0800

    142.环形链表-ii

---
+++ 142.环形链表-ii.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=typescript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function detectCycle(head: ListNode | null): ListNode | null {
+  const nodeCollection = new WeakSet()
+  while (head) {
+    if (nodeCollection.has(head)) {
+      return head
+    } else {
+      nodeCollection.add(head)
+      head = head.next
+    }
+  }
+  return null
+};
+// @lc code=end
+

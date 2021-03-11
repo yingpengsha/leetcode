12ff09cdffeeb7263dcb076f3c1a6af601eafb9b
Pengsha Ying <810998652@qq.com>
2021-03-11 20:02:57 +0800

    153.寻找旋转排序数组中的最小值

---
+++ 153.寻找旋转排序数组中的最小值.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=typescript
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+// @lc code=start
+function findMin(nums: number[]): number {
+  let min = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < min) return nums[i]
+  }
+  return min
+};
+// @lc code=end
+

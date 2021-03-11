c2e9143047c22ed796acaaea07b657a3d0f0338a
Pengsha Ying <810998652@qq.com>
2021-03-11 19:35:33 +0800

    209.长度最小的子数组

---
+++ 209.长度最小的子数组.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=typescript
+ *
+ * [209] 长度最小的子数组
+ */
+
+// @lc code=start
+function minSubArrayLen(target: number, nums: number[]): number {
+  if (nums[0] >= target) return 1
+  let start = 0
+  let end = 0
+  let sum = nums[0]
+  let max = Infinity
+  while (end < nums.length) {
+    if (sum >= target) {
+      if (end === start) return 1
+      if (end - start + 1 < max) {
+        max = end - start + 1
+      }
+      sum-= nums[start]
+      start++
+    } else {
+      end++
+      sum += nums[end]
+    }
+  }
+  return max === Infinity ? 0 : max
+};
+// @lc code=end
+

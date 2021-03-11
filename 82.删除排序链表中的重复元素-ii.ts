df7f83c73af82c6bbe55c8a0fd0655db93c6af3c
Pengsha Ying <810998652@qq.com>
2021-03-11 14:51:05 +0800

    82.删除排序链表中的重复元素-ii

---
+++ 82.删除排序链表中的重复元素-ii.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=82 lang=typescript
+ *
+ * [82] 删除排序链表中的重复元素 II
+ */
+
+// @lc code=start
+// class ListNode {
+//     val: number
+//     next: ListNode | null
+//     constructor(val?: number, next?: ListNode | null) {
+//         this.val = (val===undefined ? 0 : val)
+//         this.next = (next===undefined ? null : next)
+//     }
+// }
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head
+  let left = new ListNode(null), center = head, right = head.next
+  const newHead = left
+  let current = newHead
+  while (center) {
+    current.next = null
+    if ((left.val !== center.val) && (center.val !== (right ? right.val : null))) {
+      current = current.next = center
+    }
+    left = center
+    center = right
+    right = right ? right.next : null
+  }
+  return newHead.next
+};
+// @lc code=end
+

69fc6e965f2085f5ffac0a91bdef493476c053bb
Pengsha Ying <810998652@qq.com>
2021-03-12 10:14:49 +0800

    328.奇偶链表

---
+++ 328.奇偶链表.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=typescript
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+// class ListNode {
+//   val: number
+//   next: ListNode | null
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = (val === undefined ? 0 : val)
+//     this.next = (next === undefined ? null : next)
+//   }
+// }
+
+function oddEvenList(head: ListNode | null): ListNode | null {
+  if (!head || !head.next || !head.next.next) return head
+  let odd = head
+  let even = head.next
+  const evenHead = even
+  while (even && even.next) {
+    odd = odd.next = even.next
+    even = even.next = odd.next
+    odd.next = evenHead
+  }
+  return head
+};
+// @lc code=end
+

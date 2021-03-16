11073a17dd5c1bbcd585d06cfee32d06d7d938f2
Pengsha Ying <810998652@qq.com>
2021-03-16 14:55:31 +0800

    621.任务调度器

---
+++ 621.任务调度器.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=621 lang=typescript
+ *
+ * [621] 任务调度器
+ */
+
+// @lc code=start
+function leastInterval(tasks: string[], n: number): number {
+  const obj = {}
+  let max = 0
+  let count = 0
+  for (let i = 0; i < tasks.length; i++) {
+    obj[tasks[i]] = obj[tasks[i]] ? obj[tasks[i]] + 1 : 1
+  }
+  for (let i in obj) {
+    if (max < obj[i]) {
+      max = obj[i];
+      count = 1;
+    } else if (max == obj[i]) {
+      count++;
+    }
+  }
+  return Math.max((max - 1) * (n + 1) + count, tasks.length)
+};
+// @lc code=end
+

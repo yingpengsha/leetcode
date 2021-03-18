c5f366f1cc67e4e0ab5043ec870c5a1de201be97
Pengsha Ying <810998652@qq.com>
2021-03-18 19:58:19 +0800

    144.二叉树的前序遍历

---
+++ 144.二叉树的前序遍历.ts
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=typescript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return []
+  const result: number[] = []
+  const stack: TreeNode[] = []
+  while (true) {
+    result.push(root.val)
+    stack.push(root)
+    if (root.left) {
+      root = root.left
+    } else {
+      while (stack.length && !stack[stack.length - 1].right) {
+        stack.pop()
+      }
+      if (stack.length) {
+        root = stack.pop().right
+      } else {
+        break
+      }
+    }
+  }
+  return result
+};
+// @lc code=end
+

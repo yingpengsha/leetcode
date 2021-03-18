3133509be87a4fa290599bdc0256cfeef2a3645b
Pengsha Ying <810998652@qq.com>
2021-03-18 20:29:58 +0800

    232.用栈实现队列

---
+++ 232.用栈实现队列.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=typescript
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+    private queue: number[] = []
+
+    push(x: number): void {
+        this.queue.push(x)
+    }
+
+    pop(): number {
+        return this.queue.shift()
+    }
+
+    peek(): number {
+        return this.queue[0]
+    }
+
+    empty(): boolean {
+        return !this.queue.length
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
+

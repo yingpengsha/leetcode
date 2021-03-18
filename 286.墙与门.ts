e4e13ba718133d7c70bb271116578cfeeae8280d
Pengsha Ying <810998652@qq.com>
2021-03-18 13:52:57 +0800

    286.墙与门

---
+++ 286.墙与门.ts
@@ -0,0 +1,45 @@
+function wallsAndGates(rooms: number[][]): void {
+  const doors: number[][] = []
+  const yMax = rooms.length
+  const xMax = rooms[0].length
+  for (let i = 0; i < rooms.length; i++) {
+    for (let j = 0; j < rooms[i].length; j++) {
+      const room = rooms[i][j];
+      if (room === 0) {
+        doors.push([i, j, 1])
+      }
+    }
+  }
+  for (let i = 0; i < doors.length; i++) {
+    const [y, x, deep] = doors[i];
+    doors.push(...BFS(rooms, x, y, xMax, yMax, deep))
+  }
+};
+function BFS(rooms: number[][], x: number, y: number, xMax: number, yMax: number, deep: number) {
+  const next: number[][] = []
+  if (y > 0) {
+    if (rooms[y - 1][x] > deep) {
+      rooms[y - 1][x] = deep
+      next.push([y - 1, x, deep + 1])
+    }
+  }
+  if (x < xMax - 1) {
+    if (rooms[y][x + 1] > deep) {
+      rooms[y][x + 1] = deep
+      next.push([y, x + 1, deep + 1])
+    }
+  }
+  if (y < yMax - 1) {
+    if (rooms[y + 1][x] > deep) {
+      rooms[y + 1][x] = deep
+      next.push([y + 1, x, deep + 1])
+    }
+  }
+  if (x > 0) {
+    if (rooms[y][x - 1] > deep) {
+      rooms[y][x - 1] = deep
+      next.push([y, x - 1, deep + 1])
+    }
+  }
+  return next
+}

ce27375e4e5cb9e49e7d371ade2124d02b3fc2fa
Pengsha Ying <810998652@qq.com>
2021-03-19 15:25:38 +0800

    841.钥匙和房间

---
+++ 841.钥匙和房间.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=841 lang=typescript
+ *
+ * [841] 钥匙和房间
+ */
+
+// @lc code=start
+function canVisitAllRooms(rooms: number[][]): boolean {
+  const visitedRooms = [0]
+  const queue = [0]
+  while (queue.length) {
+    const currentRoom = queue.shift()
+    for (let i = 0; i < rooms[currentRoom].length; i++) {
+      if (!visitedRooms.includes(rooms[currentRoom][i])) {
+        queue.push(rooms[currentRoom][i])
+        visitedRooms.push(rooms[currentRoom][i])
+      }
+    }
+  }
+  return rooms.length === visitedRooms.length
+};
+// @lc code=end
+

642de65574ed7ab93318c0fed786bea88244f9b6
Pengsha Ying <810998652@qq.com>
2021-03-22 15:20:57 +0800

    636.函数的独占时间

---
+++ 636.函数的独占时间.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=636 lang=typescript
+ *
+ * [636] 函数的独占时间
+ */
+
+// @lc code=start
+function exclusiveTime(n: number, logs: string[]): number[] {
+  const runtimeMap: { [id: string]: { count: number, start: number } } = {}
+  const callStack: string[] = []
+  for (let i = 0; i < logs.length; i++) {
+    const [id, startOrEnd, tick] = logs[i].split(':');
+    if (startOrEnd === 'start') {
+      if (callStack.length) {
+        runtimeMap[callStack[callStack.length - 1]].count += +tick - runtimeMap[callStack[callStack.length - 1]].start
+      }
+      if (runtimeMap[id]) {
+        runtimeMap[id].start = +tick
+      } else {
+        runtimeMap[id] = { count: 0, start: +tick }
+      }
+      callStack.push(id)
+    } else {
+      runtimeMap[id].count += +tick - runtimeMap[id].start + 1
+      callStack.pop()
+      if (callStack.length) {
+        runtimeMap[callStack[callStack.length - 1]].start = +tick + 1
+      }
+    }
+  }
+  return Object.values(runtimeMap).map(o => o.count)
+};
+// @lc code=end
+

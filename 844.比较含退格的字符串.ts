9cf7a84c008782161e79fb42067730b6af935b01
Pengsha Ying <810998652@qq.com>
2021-03-22 11:23:41 +0800

    844.比较含退格的字符串

---
+++ 844.比较含退格的字符串.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=844 lang=typescript
+ *
+ * [844] 比较含退格的字符串
+ */
+
+// @lc code=start
+function backspaceCompare(S: string, T: string): boolean {
+  const SStack: string[] = []
+  const TStack: string[] = []
+  for (let i = 0; i < S.length; i++) {
+    if (S[i] === '#') {
+      SStack.pop()
+    } else {
+      SStack.push(S[i])
+    }
+  }
+  for (let i = 0; i < T.length; i++) {
+    if (T[i] === '#') {
+      TStack.pop()
+    } else {
+      TStack.push(T[i])
+    }
+  }
+  return SStack.join('') === TStack.join('')
+};
+// @lc code=end
+

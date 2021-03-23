3e5bf02cb62704c546b3d40126d31fb6b34dac5d
Pengsha Ying <810998652@qq.com>
2021-03-23 18:52:56 +0800

    341.扁平化嵌套列表迭代器

---
+++ 341.扁平化嵌套列表迭代器.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=341 lang=typescript
+ *
+ * [341] 扁平化嵌套列表迭代器
+ */
+
+// @lc code=start
+class NestedIterator {
+    private stack: NestedInteger[]
+    constructor(nestedList: NestedInteger[]) {
+        this.stack = nestedList
+    }
+
+    hasNext(): boolean {
+        while (this.stack.length !== 0) {
+            if (this.stack[0].isInteger()) {
+                return true;
+            } else {
+                let cur = this.stack[0].getList();
+                this.stack.shift();
+                this.stack.unshift(...cur);
+            }
+        }
+    }
+
+    next(): number {
+        return this.stack.shift().getInteger();
+    }
+}
+// @lc code=end
+

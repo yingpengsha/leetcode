cdf9bc389cecb4029e5287d490da4f5075e7719c
Pengsha Ying <810998652@qq.com>
2021-03-31 14:54:05 +0800

    283.移动零

---
+++ 283.移动零.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=typescript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - i; j++) {
+      if (nums[j] === 0 && nums[j + 1] !== 0) {
+        nums[j] ^= nums[j + 1] 
+        nums[j + 1] ^= nums[j] 
+        nums[j] ^= nums[j + 1] 
+      }
+    }
+  }
+};
+// @lc code=end
+

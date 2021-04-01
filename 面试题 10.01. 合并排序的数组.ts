88a20361a84d00c850898c3e7d143f50f0b28599
Pengsha Ying <810998652@qq.com>
2021-04-01 10:50:15 +0800

    面试题 10.01. 合并排序的数组

---
+++ 面试题 10.01. 合并排序的数组.ts
@@ -0,0 +1,18 @@
+/**
+ Do not return anything, modify A in-place instead.
+ */
+function merge(A: number[], m: number, B: number[], n: number): void {
+  let AIndex = 0
+  while (B.length && AIndex < m) {
+    if (B[0] <= A[AIndex]) {
+      A.pop()
+      m++
+      A.splice(AIndex, 0, B.shift())
+    } else {
+      AIndex++
+    }
+  }
+  if (B.length) {
+    A.splice(AIndex, B.length, ...B)
+  }
+};

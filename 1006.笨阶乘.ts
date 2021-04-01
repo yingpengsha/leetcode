3956c4760e0bfce4ae47334a4d2e750ae1029750
Pengsha Ying <810998652@qq.com>
2021-04-01 10:39:31 +0800

    1006.笨阶乘

---
+++ 1006.笨阶乘.ts
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=1006 lang=typescript
+ *
+ * [1006] 笨阶乘
+ */
+
+// @lc code=start
+function clumsy(N: number): number {
+  const signs = ['*', '/', '+', '-']
+  const numberStack = [N]
+  const signStack = []
+  for (let i = 0; i < N - 1; i++) {
+    const sign = signs[i % 4]
+    const number = N - 1 - i
+    switch (sign) {
+      case '*':
+        numberStack.push(numberStack.pop() * number)
+        break;
+      case '/':
+        numberStack.push(Math.floor(numberStack.pop() / number))
+        break;
+      default:
+        if (signStack.length) {
+          const lastSign = signStack.pop()
+          const topNumber = numberStack.pop()
+          if (lastSign === '-') {
+            numberStack.push(numberStack.pop() - topNumber)
+          } else {            
+            numberStack.push(numberStack.pop() + topNumber)
+          }
+        }
+        numberStack.push(number)
+        signStack.push(sign)
+        break;
+    }
+  }
+  if (signStack.length) {
+    const lastSign = signStack.pop()
+    const topNumber = numberStack.pop()
+    if (lastSign === '-') {
+      numberStack.push(numberStack.pop() - topNumber)
+    } else {            
+      numberStack.push(numberStack.pop() + topNumber)
+    }
+  }
+  return numberStack[0]
+};
+// @lc code=end
+

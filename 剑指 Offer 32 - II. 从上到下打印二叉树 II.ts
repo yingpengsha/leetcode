3aff659219e132625c42a236ac4182db6a422c71
Pengsha Ying <810998652@qq.com>
2021-04-06 14:43:42 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II

---
+++ 剑指 Offer 32 - II. 从上到下打印二叉树 II.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return []
+  let currentDeep = [root]
+  const result: number[][] = []
+  while (currentDeep.length) {
+    const children: TreeNode[] = []
+    result.push(currentDeep.map(node => {
+      children.push(...[node.left, node.right].filter(Boolean))
+      return node.val
+    }))
+    currentDeep = children
+  }
+  return result
+};

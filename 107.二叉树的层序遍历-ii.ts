4a94c19e842e71b92e20abce7c0ee7f7815fb8b4
Pengsha Ying <810998652@qq.com>
2021-04-06 18:03:59 +0800

    107.二叉树的层序遍历-ii

---
+++ 107.二叉树的层序遍历-ii.ts
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=107 lang=typescript
+ *
+ * [107] 二叉树的层序遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  if (!root) return []
+  let currentDeep = [root]
+  const result: number[][] = []
+  while (currentDeep.length) {
+    const children: TreeNode[] = []
+
+    result.unshift(currentDeep.map(node => {
+      if (node.left) {
+        children.push(node.left)
+      }
+      if (node.right) {
+        children.push(node.right)
+      }
+      return node.val
+    }))
+
+    currentDeep = children
+  }
+  return result
+};
+// @lc code=end
+

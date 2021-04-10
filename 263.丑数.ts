a156f9c31615ee55254f3bede80955cca77bba48
Pengsha Ying <810998652@qq.com>
2021-04-10 13:08:20 +0800

    263.丑数

---
+++ 263.丑数.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=typescript
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+function isUgly(n: number): boolean {
+  if (!n) return false
+  while (n !== 1) {
+    if (n / 2 === Math.ceil(n / 2)) {
+      n /= 2
+    } else if (n / 3 === Math.ceil(n / 3)) {
+      n /= 3
+    } else if (n / 5 === Math.ceil(n / 5)) {
+      n /= 5
+    } else {
+      return false
+    }
+  }
+  return true
+};
+// @lc code=end
+

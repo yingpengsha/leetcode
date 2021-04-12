272ae49bb06f23b9217c87def81db1d3f34bb83a
Pengsha Ying <810998652@qq.com>
2021-04-12 10:25:26 +0800

    179.最大数

---
+++ 179.最大数.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=179 lang=typescript
+ *
+ * [179] 最大数
+ */
+
+// @lc code=start
+function largestNumber(nums: number[]): string {
+  nums.sort((x, y) => {
+    let sx = 10, sy = 10;
+    while (sx <= x) {
+      sx *= 10;
+    }
+    while (sy <= y) {
+      sy *= 10;
+    }
+    return +('' + (sx * y + x)) - +('' + (sy * x + y));
+  })
+  if (nums[0] === 0) {
+    return '0';
+  }
+  return nums.join('')
+};
+// @lc code=end
+

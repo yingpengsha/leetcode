8e9be0795c4b99a766097ee49734027f686093c5
Pengsha Ying <810998652@qq.com>
2021-04-13 21:02:42 +0800

    230.二叉搜索树中第k小的元素

---
+++ 230.二叉搜索树中第k小的元素.ts
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=typescript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+  const result = []
+  const stack = [root]
+  while (stack) {
+    while (stack[stack.length - 1].left) {
+      stack.push(stack[stack.length - 1].left)
+    }
+    while (stack.length && !stack[stack.length - 1].right) {
+      result.push(stack.pop().val)
+      if (result.length === k) {
+        return result.pop()
+      }
+    }
+    if (stack.length) {
+      const top = stack.pop()
+      result.push(top.val)
+      if (result.length === k) {
+        return result.pop()
+      }
+      stack.push(top.right)
+    }
+  }
+};
+// @lc code=end
+

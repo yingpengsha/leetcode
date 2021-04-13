3e6283fd36f228be0c7c59699541345834c2211a
Pengsha Ying <810998652@qq.com>
2021-04-13 20:45:54 +0800

    300.最长递增子序列

---
+++ 300.最长递增子序列.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=typescript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+function lengthOfLIS(nums: number[]): number {
+  const dp: number[] = Array(nums.length)
+  let max = dp[0] = 1
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = 1
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[j] + 1, dp[i])
+      }
+    }
+    if (dp[i] > max) {
+      max = dp[i]
+    }
+  }
+  return max
+};
+// @lc code=end
+

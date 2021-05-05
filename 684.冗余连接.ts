2c6bbe0f2fe172f15e4ee0cdc50c06eb29d6046e
Pengsha Ying <810998652@qq.com>
2021-05-05 15:39:02 +0800

    684.冗余连接

---
+++ 684.冗余连接.ts
@@ -0,0 +1,37 @@
+ /*
+ * @lc app=leetcode.cn id=684 lang=typescript
+ *
+ * [684] 冗余连接
+ */
+
+// @lc code=start
+function findRedundantConnection(edges: number[][]): number[] {
+  class UnionMap {
+    father = new Map<number, number>()
+    
+    find(index: number): number {
+      if (this.father.has(index)) {
+        this.father.set(index, this.father.get(index) === index ? index : this.find(this.father.get(index)))
+      } else {
+        this.father.set(index, index)
+      }
+      return this.father.get(index)
+    }
+
+    merge(a: number, b: number) {
+      this.father.set(this.find(a), this.find(b))
+    }
+  }
+  const union = new UnionMap()
+  const result: number[][] = []
+  for (const [a, b] of edges) {
+    if (union.find(a) === union.find(b)) {
+      result.push([a, b])
+    } else {
+      union.merge(a, b)
+    }
+  }
+  return result.pop()
+};
+// @lc code=end
+

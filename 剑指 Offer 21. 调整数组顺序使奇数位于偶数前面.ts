6fde60216330fd9a3000f8aab0c22d251f04daea
Pengsha Ying <810998652@qq.com>
2021-05-08 17:44:25 +0800

    双路快排

--- 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.ts
+++ 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.ts
@@ -1,11 +1,15 @@
+// 双路快排
 function exchange(nums: number[]): number[] {
-  const result: number[] = []
-  for (const value of nums) {
-    if (value % 2 === 0) {
-      result.push(value)
-    } else {
-      result.unshift(value)
-    }
+  if (nums.length > 1){
+    let left = 0
+    let right = nums.length - 1
+    do {
+      while (left < nums.length && nums[left] % 2 === 1) left++
+      while (right > 0 && nums[right] % 2 === 0) right --
+      if (left < right) {
+        [nums[right--], nums[left++]] = [nums[left], nums[right]]
+      }
+    } while (left < right);
   }
-  return result
+  return nums
 };

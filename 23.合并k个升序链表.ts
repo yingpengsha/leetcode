1ec6ecf3b7e052807cd4fcde5d313a940c051164
Pengsha Ying <810998652@qq.com>
2021-06-15 09:35:28 +0800

    23.合并k个升序链表

--- 23.合并k个升序链表.ts
+++ 23.合并k个升序链表.ts
@@ -5,41 +5,98 @@
  */
 
 // @lc code=start
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
+// class ListNode {
+//   val: number
+//   next: ListNode | null
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = (val === undefined ? 0 : val)
+//     this.next = (next === undefined ? null : next)
+//   }
+// }
+class Heap<T = number> {
+  public nodes: T[] = []
+  public compare: (value1: T, value2: T) => boolean
+  constructor(initNodes: T[] = [], compare: (value1: T, value2: T) => boolean) {
+    initNodes.forEach(node => this.add(node))
+    this.compare = compare
+  }
+
+  add(value: T) {
+    this.nodes.push(value)
+    this.shiftUp(this.size - 1)
+  }
+
+  poll() {
+    if (!this.size) return undefined
+    const peekValue = this.nodes.shift()
+    if (this.size) {
+      this.nodes.unshift(this.nodes.pop())
+      this.shiftDown(0)
     }
-}
-
-function isCanReverse(head: ListNode, count:number): boolean {
-  let actualCount = 0
-  while (head && actualCount < count) {
-    actualCount++
-    head = head.next
+    return peekValue
   }
-  return actualCount >= count
-}
 
-function reverseN(head: ListNode, count: number): { head: ListNode, next: ListNode | null } {
-  if (count === 1) return { head, next: head.next }
-  const last = reverseN(head.next, --count).head
-  const lastNext = head.next.next
-  head.next.next = head
-  head.next = lastNext
-  return { 
-    head: last,
-    next: head.next
+  private shiftUp(index: number) {
+    let parentIndex = (index - 1) >> 1
+    while (index && !this.compare(this.nodes[parentIndex], this.nodes[index])) {
+      this.swap(parentIndex, index)
+      index = parentIndex
+      parentIndex = (index - 1) >> 1
+    }
+  }
+
+  private shiftDown(index: number) {
+    let left = index * 2 + 1
+    let right = (index + 1) * 2
+    while (left <= this.size - 1) {
+      let nextIndex = index
+      if (!this.compare(this.nodes[nextIndex], this.nodes[left])) {
+        nextIndex = left
+      }
+      if (right <= this.size - 1 && !this.compare(this.nodes[nextIndex], this.nodes[right])) {
+        nextIndex = right
+      }
+      if (nextIndex === index) {
+        return index
+      } else {
+        this.swap(nextIndex, index)
+        index = nextIndex
+        left = index * 2 + 1
+        right = (index + 1) * 2
+      }
+    }
+    return index
+  }
+
+  private swap(index1: number, index2: number) {
+    [this.nodes[index1], this.nodes[index2]] = [this.nodes[index2], this.nodes[index1]]
+  }
+
+  get peek() {
+    return this.nodes[0]
+  }
+
+  get size() {
+    return this.nodes.length
   }
 }
 
-function reverseKGroup(head: ListNode, count: number): ListNode {
-  if(!head || !head.next || !count || !isCanReverse(head, count)) return head
-  const { head: newHead, next: nextChunkHead } = reverseN(head, count)
-  reverseKGroup(nextChunkHead, count)
-  return newHead
-}
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+  if (lists.length === 1) return lists[0]
+  const minHeap = new Heap<ListNode>([], (node1, node2) => node1.val <= node2.val)
+  for (let head of lists) {
+    while (head) {
+      minHeap.add(head)
+      head = head.next
+    }
+  }
+  const newHead = new ListNode(null)
+  let head = newHead
+  while (minHeap.peek) {
+    head = head.next = minHeap.poll()
+    head.next = null
+  }
+  return newHead.next
+};
 // @lc code=end
 

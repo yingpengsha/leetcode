af5a3670640c87546812f0dbc0160c0e726c51eb
Pengsha Ying <810998652@qq.com>
2021-06-18 13:56:06 +0800

    274.h-指数

---
+++ 274.h-指数.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=274 lang=typescript
+ *
+ * [274] H 指数
+ */
+
+// @lc code=start
+function hIndex(citations: number[]): number {
+  const ordered = citations.sort((a, b) => b - a)
+  let max = 0
+  for (let i = 0; i < citations.length && citations[i] !== 0; i++) {
+    if (ordered[i] <= i + 1) {
+      max = Math.max(max, ordered[i])
+    } else {
+      max = Math.max(max, i + 1)
+    }
+  }
+  return max
+};
+// @lc code=end
+

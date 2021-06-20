45a16030100ef4924eafa329792235c5c293aac8
Pengsha Ying <810998652@qq.com>
2021-06-20 14:26:53 +0800

    1288.删除被覆盖区间

---
+++ 1288.删除被覆盖区间.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1288 lang=typescript
+ *
+ * [1288] 删除被覆盖区间
+ */
+
+// @lc code=start
+function removeCoveredIntervals(intervals: number[][]): number {
+  intervals = intervals.sort((a, b) => (a[0] - b[0]) || (b[1] - a[1]))
+  let count = 0
+  for (let i = 0; i < intervals.length; i++) {
+    const element = intervals[i]
+    count += 1
+    let next = i + 1
+    while (i + 1 < intervals.length && element[0] <= intervals[next][0] && element[1] >= intervals[next][1]) {
+      i += 1
+      next += 1
+    }
+  }
+  return count
+};
+// @lc code=end
+

415c9c43f1abaa8281ca0f2a70b4cdfacd5abc42
Pengsha Ying <810998652@qq.com>
2021-09-17 15:56:58 +0800

    207.课程表

---
+++ 207.课程表.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=207 lang=typescript
+ *
+ * [207] 课程表
+ */
+
+// @lc code=start
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+  const inDegreeCounts = new Array(numCourses).fill(0)
+  const outDegreeCollection: number[][] = inDegreeCounts.map(() => [])
+  const queue: number[] = []
+
+  for (let i = 0; i < prerequisites.length; i++) {
+    const [one, two] = prerequisites[i];
+    inDegreeCounts[one] += 1
+    outDegreeCollection[two].push(one)
+  }
+
+  for (let i = 0; i < inDegreeCounts.length; i++) {
+    const inDegreeCount = inDegreeCounts[i];
+    if (inDegreeCount === 0) {
+      queue.push(i)
+    }
+  }
+
+  let count = 0
+  while (queue.length) {
+    const idx = queue.shift()
+    count += 1
+    for (let i = 0; i < outDegreeCollection[idx].length; i++) {
+      const outDegreeIdx = outDegreeCollection[idx][i];
+      inDegreeCounts[outDegreeIdx] -= 1
+      if (inDegreeCounts[outDegreeIdx] === 0) {
+        queue.push(outDegreeIdx)
+      }
+    }
+  }
+  return count === numCourses
+};
+// @lc code=end
+

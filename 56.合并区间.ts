ab21cf374ecfcd55f4d237053c7195bfab31aec9
Pengsha Ying <yingfusheng@foxmail.com>
2021-10-03 15:21:58 +0800

    56.合并区间（解法二）

--- 56.合并区间.ts
+++ 56.合并区间.ts
@@ -6,17 +6,29 @@
 
 // @lc code=start
 function merge (intervals: number[][]): number[][] {
-  intervals.sort((a, b) => a[0] - b[0])
-  const ans = []
-  let index = -1
-  for (const interval of intervals) {
-    if (index == -1 || interval[0] > ans[index][1]) {
-      ans.push(interval)
-      index++
-    } else {
-      ans[index][1] = Math.max(ans[index][1], interval[1])
+  const idxOrder = new Map<number, number>()
+  for (let i = 0; i < intervals.length; i++) {
+    const [left, right] = intervals[i]
+    idxOrder.set(left, (idxOrder.get(left) || 0) + 1)
+    idxOrder.set(right, (idxOrder.get(right) || 0) - 1)
+  }
+  const sortIdx = [...idxOrder].sort((a, b) => a[0] - b[0])
+  console.log(sortIdx)
+  const result: [number, number][] = []
+  let left
+  let count = 0
+  for (let i = 0; i < sortIdx.length; i++) {
+    const [idx, order] = sortIdx[i]
+    count += order
+    if (left === undefined) {
+      left = idx
+    }
+    if (count === 0) {
+      result.push([left, idx])
+      left = undefined
+      count = 0
     }
   }
-  return ans
+  return result
 };
 // @lc code=end
